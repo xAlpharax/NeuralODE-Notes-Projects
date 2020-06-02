dbcc7d223d9be34829ff2eb493ebe91580ac5a3a
xAlpharax <iustin.raznic@gmail.com>
2020-06-02 03:28:43 +0300

    Added drcnn for cifar NOT completely

--- utils/visualization.py
+++ utils/visualization.py
@@ -7,20 +7,20 @@ sys.path.append('./')
 ######################################################
 
 def visualize(h, name):
-	graphacc = pygal.Line() #style=pygal.style.RotateStyle('#9e6ffe'), interpolate='hermite'
-	graphacc.title = 'Accuracy'
-	graphacc.add('Training Acc', h.history['accuracy'])
-	graphacc.add('Testing Acc', h.history['val_accuracy'])
+    graphacc = pygal.Line() #style=pygal.style.RotateStyle('#9e6ffe'), interpolate='hermite'
+    graphacc.title = 'Accuracy'
+    graphacc.add('Training Acc', h.history['accuracy'])
+    graphacc.add('Testing Acc', h.history['val_accuracy'])
 
-	graphacc.render_to_file('assets/visual/' + name + '-Accuracy.svg')
+    graphacc.render_to_file('assets/visual/' + name + '-Accuracy.svg')
 
-	######################################################
+    ######################################################
 
-	graphloss = pygal.Line() #style=pygal.style.RotateStyle('#9e6ffe'), interpolate='hermite'
-	graphloss.title = 'Loss'
-	graphloss.add('Training Loss', h.history['loss'])
-	graphloss.add('Testing Loss', h.history['val_loss'])
+    graphloss = pygal.Line() #style=pygal.style.RotateStyle('#9e6ffe'), interpolate='hermite'
+    graphloss.title = 'Loss'
+    graphloss.add('Training Loss', h.history['loss'])
+    graphloss.add('Testing Loss', h.history['val_loss'])
 
-	graphloss.render_to_file('assets/visual/' + name + '-Loss.svg')
+    graphloss.render_to_file('assets/visual/' + name + '-Loss.svg')
 
-	return
+    return
