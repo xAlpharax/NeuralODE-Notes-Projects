7b9ec0dc8b45354fa118942cf88244a2842879c1
xAlpharax <iustin.raznic@gmail.com>
2020-07-11 01:16:51 +0300

    Added preprocess

---
+++ utils/preprocess.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import numpy as np
+
+def preprocessceleb(img_path):
+    img_arr = Image.open(img_path)
+    img_arr = img_arr.convert('RGB')
+    img_arr = img_arr.resize((90, 110))
+
+    # Preprocessing the image
+    img_arr = np.asarray(img_arr)
+    img_arr = (img_arr / 127.5) - 1
+
+    # Make 4D tensor out of the image
+    img_arr = np.expand_dims(img_arr, axis=0)
+
+    return img_arr
