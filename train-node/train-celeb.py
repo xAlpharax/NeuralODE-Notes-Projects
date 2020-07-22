adbd8d1c3a9eee0cc575c09650d86bc2b70db093
xAlpharax <iustin.raznic@gmail.com>
2020-07-22 23:55:02 +0300

    batch&epochs

--- train-node/train-celeb.py
+++ train-node/train-celeb.py
@@ -9,8 +9,8 @@ train = celeb.load()
 test = celeb.testSet()
 
 total_size = len(train[0])
-print(total_size
-  )
+print(total_size)
+
 print("Train Data: {}".format(train[0].shape))
 print("Test Data: {}".format(test[0].shape))
 
@@ -50,7 +50,7 @@ def DCODNN(input_shape, num_classes):
 
 DCODNN = DCODNN((110, 90, 3), 2)
 
-batch_size = 512
+batch_size = 128
 epochs = 30
 
 import numpy as np
