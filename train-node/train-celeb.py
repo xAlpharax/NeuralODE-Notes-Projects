b719968fc27ffb7b861b27cb1dada7c872b273d5
xAlpharax <iustin.raznic@gmail.com>
2020-07-26 01:43:26 +0300

    network modif

--- train-node/train-celeb.py
+++ train-node/train-celeb.py
@@ -28,9 +28,9 @@ def DCODNN(input_shape, num_classes):
   y = Conv2D(32, (3,3), activation='relu')(x)
   y = BatchNormalization(axis=-1)(y)
   y = MaxPooling2D(2,2)(y)
-  y = Dropout(0.4)(y)
+  y = Dropout(0.3)(y)
 
-  y = Conv2D(128, (3,3), activation='relu')(y)
+  y = Conv2D(64, (3,3), activation='relu')(y)
   y = BatchNormalization(axis=-1)(y)
   y = Conv2D(128, (3,3), activation='relu')(y)
   y = BatchNormalization(axis=-1)(y)
